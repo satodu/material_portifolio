26669452a709c99dee154a72629c2c62361fc208
Dom <sato.du@gmail.com>
2015-11-09 09:52:48 -0200

    add  main files

---
+++ head_elements.php
@@ -0,0 +1,14 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link rel="stylesheet" href="css/materialize.min.css">
+      <link rel="stylesheet" href="css/css.css">
+      <link rel="stylesheet" href="//cdn.materialdesignicons.com/1.2.65/css/materialdesignicons.min.css">
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+      <script src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/js.js"></script>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Potifolio</title>
+<? include 'head_elements.php' ?>
+</head>
+<body>
+
+</body>
+</html>
